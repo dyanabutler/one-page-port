49a0907c5094f658d2b6f2b9ef37f66197c56674
Dyana Butler <dyanajbutler98@gmail.com>
2024-09-05 20:50:44 -0400

    added link to contact button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -88,7 +88,7 @@ export default function Header() {
           {/* "Contact" Button */}
           <div>
             <a
-              href="#_"
+              href="mailto:0xdyana@gmail.com"
               className="relative inline-block px-4 py-2 font-medium group "
             >
               <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0 rounded-sm"></span>
